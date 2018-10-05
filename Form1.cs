035d8c51931513a2f4c1a7ebf3326336e760aad3
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-10-05 15:37:34 +0900

    起動時にInternet Explorerプロセス一覧を表示する

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,29 @@ namespace KillAllIeProcesses
 {
     public partial class Form1 : Form
     {
+        private Process[] Processes;
+        private static readonly string IeProcName = "iexplore";
+
         public Form1()
         {
             InitializeComponent();
         }
-        private void Form1_Load(object sender, System.EventArgs e) { }
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            ListViewItem itemAdd;
+            ListView1.Items.Clear();
+            Processes = Process.GetProcessesByName(IeProcName);
+            foreach (Process proc in Processes)
+            {
+                var title = proc.MainWindowTitle;
+                if (title.Length == 0)
+                {
+                    title = "Internet Explorer";
+                }
+                itemAdd = ListView1.Items.Add(title);
+                itemAdd.SubItems.Add(proc.Id.ToString());
+            }
+        }
         private void btnClose1_Click(object sender, System.EventArgs e) { }
         private void btnCloseAll_Click(object sender, System.EventArgs e) { }
         private void closing(object sender, System.ComponentModel.CancelEventArgs e) { }
